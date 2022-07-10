c6f154ae70db271c82cd55dfba40b0d4194ebe09
Lauri92 <55086903+Lauri92@users.noreply.github.com>
2022-07-10 22:48:22 +0300

    Composable permissions

--- app/src/main/java/fi/lauriari/compose_camera/MainActivity.kt
+++ app/src/main/java/fi/lauriari/compose_camera/MainActivity.kt
@@ -1,90 +1,24 @@
 package fi.lauriari.compose_camera
 
 import android.Manifest
-import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.Modifier
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import coil.compose.rememberImagePainter
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import fi.lauriari.compose_camera.permissions.CameraContent
 import fi.lauriari.compose_camera.ui.theme.ComposeCameraTheme
 
 class MainActivity : ComponentActivity() {
-
-    private var shouldShowCamera = mutableStateOf(false)
-
-    private lateinit var photoUri: Uri
-    private var shouldShowPhoto = mutableStateOf(false)
-
-    private val requestPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestPermission()
-    ) { isGranted ->
-        if (isGranted) {
-            Log.i("kilo", "Permission granted")
-            shouldShowCamera.value = true
-        } else {
-            Log.i("kilo", "Permission denied")
-        }
-    }
-
-
+    @OptIn(ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeCameraTheme {
-
-                if (shouldShowCamera.value) {
-                    CameraView(
-                        onImageCaptured = ::handleImageCapture,
-                        onError = { Log.e("kilo", "View error:", it) }
-                    )
-                }
-
-                if (shouldShowPhoto.value) {
-                    Image(
-                        painter = rememberImagePainter(photoUri),
-                        contentDescription = null,
-                        modifier = Modifier.fillMaxSize()
-                    )
-                }
+                CameraContent(permission = Manifest.permission.CAMERA)
             }
         }
-        requestCameraPermission()
-    }
-
-    private fun requestCameraPermission() {
-        when {
-            ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.CAMERA
-            ) == PackageManager.PERMISSION_GRANTED -> {
-                Log.i("kilo", "Permission previously granted")
-                shouldShowCamera.value = true
-            }
-
-            ActivityCompat.shouldShowRequestPermissionRationale(
-                this,
-                Manifest.permission.CAMERA
-            ) -> Log.i("kilo", "Show camera permissions dialog")
-
-            else -> requestPermissionLauncher.launch(Manifest.permission.CAMERA)
-        }
-    }
-
-    private fun handleImageCapture(uri: Uri) {
-        Log.i("kilo", "Image captured: $uri")
-
-        shouldShowCamera.value = false
-
-        photoUri = uri
-        shouldShowPhoto.value = true
     }
 }
