8de0c96a2ba58ab0395caf21c403ced2c1f5c883
Lauri92 <55086903+Lauri92@users.noreply.github.com>
2022-07-16 19:24:55 +0300

    Simple listing of files in device

--- app/src/main/java/fi/lauriari/compose_camera/composables/MainContent.kt
+++ app/src/main/java/fi/lauriari/compose_camera/composables/MainContent.kt
@@ -1,21 +1,32 @@
 package fi.lauriari.compose_camera.composables
 
+import android.content.Context
 import android.net.Uri
+import android.util.Log
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
 import coil.compose.rememberAsyncImagePainter
-import com.google.accompanist.permissions.*
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberPermissionState
 import fi.lauriari.compose_camera.permissions.HandleRequest
 import fi.lauriari.compose_camera.permissions.PermissionDeniedContent
+import java.io.File
 
 @ExperimentalPermissionsApi
 @Composable
@@ -26,9 +37,11 @@ fun MainContent(
 
     val config = LocalConfiguration.current
     config.smallestScreenWidthDp
+    val context = LocalContext.current
 
     var shouldShowCamera by rememberSaveable { mutableStateOf(false) }
     var shouldShowPhoto by rememberSaveable { mutableStateOf(false) }
+    var shouldShowImageList by rememberSaveable { mutableStateOf(false) }
     var photoUri by rememberSaveable { mutableStateOf("".toUri()) }
 
     HandleRequest(
@@ -63,7 +76,11 @@ fun MainContent(
                 ShowButton(
                     onClick = {
                         shouldShowCamera = true
-                    }
+                    },
+                    onClick2 = {
+                        shouldShowImageList = true
+                    },
+                    shouldShowImageList = shouldShowImageList
                 )
             }
         }
@@ -75,6 +92,8 @@ fun ShowPhotoAndButton(
     onClick: () -> Unit,
     photoUri: Uri?,
 ) {
+
+    Log.d("photouritest", photoUri.toString())
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -93,8 +112,7 @@ fun ShowPhotoAndButton(
         ) {
             Button(
                 onClick = onClick
-            )
-            {
+            ) {
                 Text("Toggle Camera")
             }
         }
@@ -102,16 +120,60 @@ fun ShowPhotoAndButton(
 }
 
 @Composable
-fun ShowButton(onClick: () -> Unit) {
-    Box(
+fun ShowButton(
+    onClick: () -> Unit,
+    onClick2: () -> Unit,
+    shouldShowImageList: Boolean
+) {
+
+    val context = LocalContext.current
+
+    Column(
         modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+
     ) {
-        Button(
-            onClick = onClick
-        )
-        {
-            Text("Toggle Camera")
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Button(
+                onClick = onClick
+            ) {
+                Text("Toggle Camera")
+            }
+            Button(
+                onClick = onClick2
+            ) {
+                Text("Images")
+            }
+        }
+
+        if (shouldShowImageList) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState()),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Log.d("filestest", "Got here")
+                val imageDir: File = context.getDir("imageDir", Context.MODE_PRIVATE)
+                val directory: File = imageDir.absoluteFile
+
+                for (file in directory.list()!!) {
+                    Log.d("filestest", file)
+                    val fileUri: Uri =
+                        "file:///data/user/0/fi.lauriari.compose_camera/app_imageDir/$file".toUri()
+                    Image(
+                        painter = rememberAsyncImagePainter(fileUri),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(150.dp)
+                            .padding(20.dp)
+                    )
+                }
+            }
         }
     }
 }
